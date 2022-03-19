61c409015a5b6d89a67a76df1ad02cfd1f7dea88
zander <zander.ewell0759@stu.mtec.edu>
2022-03-19 15:30:54 -0600

    This is a webpage that hss a Toggle Dark Mode button and shows any items that are in store

---
+++ script.js
@@ -0,0 +1,89 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+storeItems.forEach(function(item){
+    if(item.inStock === true){
+    $('#appendToMe').append(`<div class= "p"> <p> $${item.price} </p> <p> ${item.name} </p>  <p>${item.details} </p> </div> `).attr('price','item').addClass('inStock')
+    }
+    else{
+    $('#appendToMe').addClass('notInStock')
+    }
+    });
+
+
+
+
+
+let button = document.getElementById('#clickMe');
+clickMe.addEventListener('click',function() {
+  console.log('click')
+
+$("p").each(function(index) {
+$("#contentContainer").toggleClass("darkMode");
+});
+});
